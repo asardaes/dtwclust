593b97cf7d5faf52ce425f4e7c8722189a467b48
asardaes <alexis.sarda@gmail.com>
2015-12-30 00:55:08 +0100

    Test for symmetric optimization

--- tests/testthat/test_custom_distance.R
+++ tests/testthat/test_custom_distance.R
@@ -14,13 +14,30 @@ if (!pr_DB$entry_exists("nDTW"))
                             loop = TRUE, type = "metric", distance = TRUE,
                             description = "Normalized DTW with L1 norm")
 
-pc_ndtw <- dtwclust(data_subset, k = 4, distance = "nDTW",
+pc_ndtw <- dtwclust(data_list[1:20], k = 4, distance = "nDTW",
                     control = ctrl, seed = 8319)
 
 pc_ndtw <- reset_nondeterministic(pc_ndtw)
 
 test_that("custom distance gives the same result as reference",
           my_expect_equal_to_reference(pc_ndtw))
+
+# =================================================================================================
+# A symmetric computation of the above
+# =================================================================================================
+
+ctrl@symmetric <- TRUE
+
+pc_ndtw <- dtwclust(data_list[1:20], k = 4, distance = "nDTW",
+                    control = ctrl, seed = 8319)
+
+pc_ndtw <- reset_nondeterministic(pc_ndtw)
+pc_ndtw@control@symmetric <- FALSE # just for the check below
+
+test_that("custom SYMMETRIC distance gives the same result as NON-SYMMETRIC when appropriate",
+          my_expect_equal_to_reference(pc_ndtw))
+
+ctrl@symmetric <- FALSE
 
 # =================================================================================================
 # Registered with proxy and with custom arguments
