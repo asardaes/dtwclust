717bb5c39634b6535df3396dd6127f26f07a260e
Alexis Sarda <alexis.sarda@gmail.com>
2016-09-08 14:05:53 +0200

    Adjusted repetitions test

--- tests/testthat/test_repetitions.R
+++ tests/testthat/test_repetitions.R
@@ -1,6 +1,6 @@
 context("Test repetitions")
 
-ctrl@nrep <- 8L
+ctrl@nrep <- 4L
 
 # =================================================================================================
 # Precomputing distmat
@@ -22,16 +22,28 @@ test_that("Repetitions with pam.precompute give the same result as reference",
 # Without precomputing distmat
 # =================================================================================================
 
-rep_median <- dtwclust(data_matrix, type = "partitional", k = 19:20,
-                       distance = "L1", centroid = "median",
+ctrl@pam.precompute <- FALSE
+
+rep_npampre <- dtwclust(data_matrix, type = "partitional", k = 20:21,
+                       distance = "L1", centroid = "pam",
                        preproc = NULL, control = ctrl, seed = 321)
 
-rep_median <- lapply(rep_median, reset_nondeterministic)
+rep_npampre <- lapply(rep_npampre, function(x) {
+     x <- reset_nondeterministic(x)
+     x@control@pam.precompute <- TRUE
+     x
+})
+
+rep_pampre <- lapply(rep_pampre, function(x) {
+     x@distmat <- NULL
+     x
+})
 
 test_that("Repetitions without pam.precompute give appropriate length",
-          expect_identical(length(rep_median), ctrl@nrep * 2L))
+          expect_identical(length(rep_npampre), ctrl@nrep * 2L))
 
-test_that("Repetitions without pam.precompute give the same result as reference",
-          my_expect_equal_to_reference(rep_median))
+test_that("Repetitions without pam.precompute give the same result as with pam.precompute (adjusted distmat)",
+          expect_identical(rep_pampre, rep_npampre))
 
 ctrl@nrep <- 1L
+ctrl@pam.precompute <- TRUE
