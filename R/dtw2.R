ce24f2b619b79bf30a903bcf2722840acddd08e2
Alexis Sarda <alexis.sarda@gmail.com>
2016-09-19 01:18:53 +0200

    Updated dtw2 documentation

--- R/dtw2.R
+++ R/dtw2.R
@@ -16,6 +16,12 @@
 #'
 #' This wrapper simply returns the appropriate DTW distance using L2 norm (Euclidean norm).
 #'
+#' The windowing constraint uses a centered window. The calculations expect a value in \code{window.size}
+#' that represents the distance between the point considered and one of the edges of the window. Therefore,
+#' if, for example, \code{window.size = 10}, the warping for an observation \eqn{x_i} considers the points
+#' between \eqn{x_{i-10}} and \eqn{x_{i+10}}, resulting in \code{10(2) + 1 = 21} observations falling within
+#' the window.
+#'
 #' @param x,y A time series. A multivariate series should have time spanning the rows and variables spanning
 #' the columns.
 #' @param ... Further arguments for \code{\link[dtw]{dtw}}.
