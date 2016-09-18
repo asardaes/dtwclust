cfc0b229560a9c01b6e70f30064ef3a1ed77b57c
asardaes <alexis.sarda@gmail.com>
2016-09-19 01:55:02 +0200

    Fixed CHECK complaints for dtw_basic

--- R/dtw-basic.R
+++ R/dtw-basic.R
@@ -21,7 +21,7 @@
 #' @param step.pattern Step pattern for DTW. Only \code{symmetric1} or \code{symmetric2} supported here. See
 #' \code{\link[dtw]{stepPattern}}.
 #' @param backtrack Also compute the warping path between series? See details.
-#' @param Normalize Should the distance be normalized? Only supported for \code{symmetric2}.
+#' @param normalize Should the distance be normalized? Only supported for \code{symmetric2}.
 #'
 #' @return The DTW distance. For \code{backtrack} \code{=} \code{TRUE}, a list with: \itemize{
 #'   \item \code{distance}: The DTW distance.
@@ -32,7 +32,7 @@
 #' @export
 #'
 dtw_basic <- function(x, y, window.size = NULL, norm = "L1",
-                      step.pattern = symmetric2, backtrack = FALSE, normalize = FALSE) {
+                      step.pattern = get("symmetric2"), backtrack = FALSE, normalize = FALSE) {
      consistency_check(x, "ts")
      consistency_check(y, "ts")
 
@@ -49,9 +49,9 @@ dtw_basic <- function(x, y, window.size = NULL, norm = "L1",
      norm <- match.arg(norm, c("L1", "L2"))
      norm <- switch(norm, "L1" = 1, "L2" = 2)
 
-     if (identical(step.pattern, symmetric1))
+     if (identical(step.pattern, get("symmetric1")))
           step.pattern <- 1
-     else if (identical(step.pattern, symmetric2))
+     else if (identical(step.pattern, get("symmetric2")))
           step.pattern <- 2
      else
           stop("step.pattern must be either symmetric1 or symmetric2")
@@ -78,14 +78,14 @@ dtw_basic <- function(x, y, window.size = NULL, norm = "L1",
 }
 
 dtw_basic_proxy <- function(x, y, window.size = NULL, norm = "L1",
-                            step.pattern = symmetric2, backtrack = FALSE, normalize = FALSE) {
+                            step.pattern = get("symmetric2"), backtrack = FALSE, normalize = FALSE) {
      dtw_basic(x, y, window.size = window.size,
                norm = norm, step.pattern = step.pattern,
                backtrack = FALSE, normalize = normalize)
 }
 
 dtw_dba <- function(x, y, window.size = NULL, norm = "L1",
-                    step.pattern = symmetric2, backtrack = TRUE,
+                    step.pattern = get("symmetric2"), backtrack = TRUE,
                     normalize = FALSE, lcm_gcm = NULL, ...) {
      if (is.null(lcm_gcm))
           return(dtw_basic(x, y, window.size = window.size, norm = norm,
@@ -108,9 +108,9 @@ dtw_dba <- function(x, y, window.size = NULL, norm = "L1",
      norm <- match.arg(norm, c("L1", "L2"))
      norm <- switch(norm, "L1" = 1, "L2" = 2)
 
-     if (identical(step.pattern, symmetric1))
+     if (identical(step.pattern, get("symmetric1")))
           step.pattern <- 1
-     else if (identical(step.pattern, symmetric2))
+     else if (identical(step.pattern, get("symmetric2")))
           step.pattern <- 2
      else
           stop("step.pattern must be either symmetric1 or symmetric2")
