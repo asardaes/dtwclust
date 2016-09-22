679bc67d5a9cd1e71c3aa65ecfb8c75c0531484d
asardaes <alexis.sarda@gmail.com>
2016-09-22 16:37:31 +0200

    Scale already handles matrices
    But na.rm can break too many things, better let the user deal with that.

--- R/zscore.R
+++ R/zscore.R
@@ -6,35 +6,35 @@
 #' @param x Data to normalize. Either a vector, a matrix/data.frame where each row is to be normalized, or a list of
 #' vectors.
 #' @param ... Further arguments to pass to \code{\link[base]{scale}}.
-#' @param na.rm Logical flag. Should \code{NA}s be removed? Ignored for matrix input.
 #' @param multivariate Is \code{x} a multivariate time series? It will be detected automatically if a list is provided in
 #' \code{x}.
+#' @param na.rm Deprecated
 #'
 #' @return Normalized data in the same format as provided.
 #'
 #' @export
 #'
 
-zscore <- function(x, ..., na.rm = FALSE, multivariate = FALSE) {
+zscore <- function(x, ..., na.rm, multivariate = FALSE) {
+     if (!missing(na.rm))
+          warning("The 'na.rm' has been deprecated.")
+
      if (is.list(x)) {
-          x <- lapply(x, zscore, na.rm = na.rm, multivariate = !is.null(dim(x[[1L]])), ...)
+          x <- lapply(x, zscore, multivariate = !is.null(dim(x[[1L]])), ...)
 
      } else if (!multivariate && (is.matrix(x) || is.data.frame(x))) {
-          x <- t(apply(x, 1L, zscore, na.rm = na.rm, ...))
+          x <- t(apply(x, 1L, zscore, ...))
 
      } else {
           consistency_check(x, "ts")
 
-          if (multivariate) {
-               x <- apply(x, 2L, zscore, na.rm = na.rm, ...)
+          x <- scale(x, ...)
+          x[is.nan(x)] <- 0
 
-          } else {
-               if (na.rm) x <- x[!is.na(x)]
-
-               x <- scale(x, ...)
-               x <- as.numeric(x) # scale returns columns
-               x[is.nan(x)] <- 0
-          }
+          if (multivariate)
+               attr(x, "scaled:center") <- attr(x, "scaled:scale") <- NULL
+          else
+               x <- as.numeric(x) # remove dimension and other attributes
      }
 
      x
