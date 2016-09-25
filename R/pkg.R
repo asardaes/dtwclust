1a2450e9678c610364a6d4c831d08403b288e4b9
Alexis Sarda <alexis.sarda@gmail.com>
2016-09-25 21:09:45 +0200

    packageVersion() comes from utils

--- R/pkg.R
+++ R/pkg.R
@@ -124,6 +124,7 @@
 #' @importFrom rngtools RNGseq
 #' @importFrom rngtools setRNG
 #' @importFrom Rcpp evalCpp
+#' @importFrom utils packageVersion
 #'
 NULL
 
@@ -179,8 +180,8 @@ NULL
                            'To read the included vignette, type: vignette("dtwclust").\n',
                            'Please see news(package = "dtwclust") for important information!\n')
 
-     if (grepl(".9000", packageVersion("dtwclust")))
-          message("This is a developer version of 'dtwclust'. Note that the CHECK tests fail due to parallelization.")
+     if (grepl(".9000", utils::packageVersion("dtwclust")))
+          packageStartupMessage("This is a developer version of 'dtwclust'. Note that the CHECK tests fail due to parallelization.")
 }
 
 .onUnload <- function(libpath) {
