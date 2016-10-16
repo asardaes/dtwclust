876caeb085a16d7632cfb55a2f864d3e5921d209
Alexis Sarda <alexis.sarda@gmail.com>
2016-10-16 15:14:23 +0200

    Skip FORKs on windows
    Simpler

--- tests/testthat/test_zparallel.R
+++ tests/testthat/test_zparallel.R
@@ -35,21 +35,19 @@ test_that("Parallel computation gives the same results as sequential", {
     stopImplicitCluster()
     registerDoSEQ()
 
-    skip_on_os("mac")
+    skip_on_os(c("mac", "windows"))
 
     ## Also test FORK in Unix
-    if (tolower(Sys.info()[["sysname"]]) != "windows") {
-        cat("Test FORKs:\n")
+    cat("Test FORKs:\n")
 
-        rm(cl)
-        cl <- makeCluster(num_workers, "FORK")
-        registerDoParallel(cl)
+    rm(cl)
+    cl <- makeCluster(num_workers, "FORK")
+    registerDoParallel(cl)
 
-        ## Filter excludes files that have "parallel" in them, otherwise it would be recursive
-        test_dir("./", filter = "^(?!.*parallel).*$", perl = TRUE)
+    ## Filter excludes files that have "parallel" in them, otherwise it would be recursive
+    test_dir("./", filter = "^(?!.*parallel).*$", perl = TRUE)
 
-        stopCluster(cl)
-        stopImplicitCluster()
-        registerDoSEQ()
-    }
+    stopCluster(cl)
+    stopImplicitCluster()
+    registerDoSEQ()
 })
