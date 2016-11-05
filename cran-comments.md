56bf8c6a06af5d329eedd68fccaf8e2a77549826
asardaes <alexis.sarda@gmail.com>
2016-11-05 18:47:23 +0100

    Duplicate classes aren't a problem

--- cran-comments.md
+++ cran-comments.md
@@ -16,4 +16,3 @@
 ## R CMD check results
 * There were no ERRORs or WARNINGs
 * There was a NOTE regarding spelling: TADPole is the name of an algorithm, and it is written as such. Centroid/Partitional are maybe domain specific, but are written like that too.
-* The "testthat" package has its own definition of 'proc_time' class, so said class is duplicated during tests, and only in that situation.
--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,7 @@ library(testthat) # testthat has its own definition of "proc_time"...
 library(dtwclust)
 
 #' To test in a local machine:
-#' Sys.setenv(NOT_CRAN = "true"); suppressMessages(test_dir("tests/testthat/"))
+#' Sys.setenv(NOT_CRAN = "true"); test_dir("tests/testthat/")
 #' OR
 #' devtools::test()
 #'
@@ -10,4 +10,4 @@ library(dtwclust)
 #'
 #' options(skip_par_tests = TRUE)
 #'
-test_check("dtwclust")
+testthat::test_check("dtwclust")
