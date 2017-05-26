dd32f4e03a3cf49e98475eadba552600c1ae2243
asardaes <alexis.sarda@gmail.com>
2017-05-26 17:07:29 +0200

    Use CALLDEFpp define for C++ functions

--- src/init.cpp
+++ src/init.cpp
@@ -3,12 +3,13 @@
 #include "dtwclustpp.h"
 
 #define CALLDEF(name, n) { "C_"#name, (DL_FUNC) &name, n }
+#define CALLDEFpp(name, n) { "C_"#name, (DL_FUNC) &dtwclust::name, n }
 
 static R_CallMethodDef callMethods[] = {
-    { "C_dba", (DL_FUNC) &dtwclust::dba, 8 },
-    { "C_envelope", (DL_FUNC) &dtwclust::envelope, 2 },
-    { "C_lbk", (DL_FUNC) &dtwclust::lbk, 4 },
-    { "C_lbi", (DL_FUNC) &dtwclust::lbi, 6 },
+    CALLDEFpp(dba, 8),
+    CALLDEFpp(envelope, 2),
+    CALLDEFpp(lbk, 4),
+    CALLDEFpp(lbi, 6),
     CALLDEF(dtw_basic, 10),
     CALLDEF(logGAK, 8),
     CALLDEF(pairs, 2),
@@ -19,15 +20,15 @@ static R_CallMethodDef callMethods[] = {
 void register_functions() {
     using namespace dtwclust;
 
-    #define DTWCLUST_REGISTER(__FUN__) R_RegisterCCallable("dtwclust", #__FUN__, (DL_FUNC)__FUN__);
-    DTWCLUST_REGISTER(dba)
-    DTWCLUST_REGISTER(dtw_basic)
-    DTWCLUST_REGISTER(envelope)
-    DTWCLUST_REGISTER(lbk)
-    DTWCLUST_REGISTER(lbi)
-    DTWCLUST_REGISTER(logGAK)
-    DTWCLUST_REGISTER(pairs)
-    DTWCLUST_REGISTER(setnames_inplace)
+    #define DTWCLUST_REGISTER(__FUN__) R_RegisterCCallable("dtwclust", #__FUN__, (DL_FUNC)__FUN__)
+    DTWCLUST_REGISTER(dba);
+    DTWCLUST_REGISTER(dtw_basic);
+    DTWCLUST_REGISTER(envelope);
+    DTWCLUST_REGISTER(lbk);
+    DTWCLUST_REGISTER(lbi);
+    DTWCLUST_REGISTER(logGAK);
+    DTWCLUST_REGISTER(pairs);
+    DTWCLUST_REGISTER(setnames_inplace);
     #undef DTWCLUST_REGISTER
 }
 
