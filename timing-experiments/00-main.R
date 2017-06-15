77438deb2d0b6efeb7d2db6c789148534b11f056
asardaes <alexis.sarda@gmail.com>
2017-06-15 16:52:40 +0200

    Time main in experiments

--- timing-experiments/00-main.R
+++ timing-experiments/00-main.R
@@ -6,5 +6,9 @@ library("doParallel")
 short_experiments <- TRUE
 length_diff_threshold <- 20L
 
+tic <- proc.time()
 if (file.exists("read-csv.RData")) load("read-csv.RData") else source("10-read-csv.R")
 if (file.exists("dist-results.RData")) load("dist-results.RData") else source("20-distance-experiments.R")
+toc <- proc.time() - tic
+
+message("Finished after: ", toc["elapsed"])
