e49a423fb0c2221bea42d10d8897b2d89a405955
asardaes <alexis.sarda@gmail.com>
2017-06-24 14:54:15 +0200

    Add TADPole experiments

--- timing-experiments/00-main.R
+++ timing-experiments/00-main.R
@@ -15,6 +15,7 @@ tic <- proc.time()
 if (file.exists("read-csv.RData")) load("read-csv.RData") else source("10-read-csv.R")
 if (file.exists("dist-results.RData")) load("dist-results.RData") else source("20-distance-experiments.R")
 if (file.exists("cent-results.RData")) load("cent-results.RData") else source("30-prototyping-experiments.R")
+if (file.exists("tadpole-results.RData")) load("tadpole-results.RData") else source("40-tadpole-experiments.R")
 toc <- proc.time() - tic
 
-message("Finished after: ", toc["elapsed"], " seconds")
+message("\nFinished after: ", toc["elapsed"], " seconds")
