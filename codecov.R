a5a3009e16106d603140703bcced7cf01ecc46f9
asardaes <alexis.sarda@gmail.com>
2017-07-02 14:56:07 +0200

    Ignore kcca.list()'s coverage

--- codecov.R
+++ codecov.R
@@ -5,5 +5,6 @@ codecov(type = "all",
             "R/create-dtwclust.R",
             "R/dtwclust-classes.R",
             "R/dtwclust-methods.R",
-            "R/compute-envelope.R" = 55L:58L
+            "R/compute-envelope.R" = 55L:58L,
+            "R/partitional.R" = 130L:253L
         ))
