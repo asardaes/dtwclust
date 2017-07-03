10faf00ee48a0bd6a5da2cde75e8e6e20b910c76
asardaes <alexis.sarda@gmail.com>
2017-07-04 00:53:59 +0200

    Remove unnecessary argument

--- tests/testthat/acceptance/gak.R
+++ tests/testthat/acceptance/gak.R
@@ -15,7 +15,7 @@ test_that("Symmetric univariate GAK distance gives expected results.", {
     D1 <- proxy::dist(data_subset, method = "gak", window.size = 18L)
     sigma <- attr(D1, "sigma")
     D2 <- proxy::dist(data_subset, data_subset, method = "gak", window.size = 18L, sigma = sigma)
-    D3 <- proxy::dist(data_subset, data_subset, method = "gak",window.size = 18L, sigma = sigma, symmetric = TRUE)
+    D3 <- proxy::dist(data_subset, data_subset, method = "gak", window.size = 18L, sigma = sigma)
     D4 <- sapply(data_subset, GAK, y = data_subset[[1L]], window.size = 18L, sigma = sigma)
 
     expect_equal(D1, D2, info = "single-double-arg", tolerance = 0, check.attributes = FALSE)
@@ -27,7 +27,7 @@ test_that("Symmetric multivariate GAK distance gives expected results.", {
     D1 <- proxy::dist(data_multivariate, method = "gak", window.size = 18L)
     sigma <- attr(D1, "sigma")
     D2 <- proxy::dist(data_multivariate, data_multivariate, method = "gak", window.size = 18L, sigma = sigma)
-    D3 <- proxy::dist(data_multivariate, data_multivariate, method = "gak", window.size = 18L, sigma = sigma, symmetric = TRUE)
+    D3 <- proxy::dist(data_multivariate, data_multivariate, method = "gak", window.size = 18L, sigma = sigma)
     D4 <- sapply(data_multivariate, GAK, y = data_multivariate[[1L]], window.size = 18L, sigma = sigma)
 
     expect_equal(D1, D2, info = "single-double-arg", tolerance = 0, check.attributes = FALSE)
