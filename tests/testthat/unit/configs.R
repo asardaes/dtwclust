8bc45103af190ef24d438b331811350c1769bdb7
asardaes <alexis.sarda@gmail.com>
2017-07-08 11:14:59 +0200

    Adjust configs unit tests

--- tests/testthat/unit/configs.R
+++ tests/testthat/unit/configs.R
@@ -11,7 +11,7 @@ ols <- ls()
 # pdc_configs
 # =================================================================================================
 
-test_that("Using pdc_configs works as expected.", {
+test_that("Using pdc_configs() works as expected.", {
     expect_warning(pdc_configs("d", tadpole = list(a = "a")))
 
     share_base <- c("partitional", "hierarchical", "fuzzy", "tadpole")
@@ -38,59 +38,55 @@ test_that("Using pdc_configs works as expected.", {
         tadpole = pdc_t
     )
 
-    expect_true(all(
-        times(100L) %do% {
-            this_args <- sample(pdc_args)[1L:sample(length(pdc_args), 1L)]
-            this_share <- sample(share_base)[1L:sample(length(share_base), 1L)]
+    nrows_is_correct <- times(100L) %do% {
+        this_args <- sample(pdc_args)[1L:sample(length(pdc_args), 1L)]
+        this_share <- sample(share_base)[1L:sample(length(share_base), 1L)]
+        type <- sample(c("preproc", "distance", "centroid"), 1L)
 
-            type <- sample(c("preproc", "distance", "centroid"), 1L)
-
-            if (type == "distance") {
-                this_args$tadpole <- NULL
-                this_share <- this_share[setdiff(names(this_share), "tadpole")]
-            }
-
-            if (length(this_args) == 0L || length(this_share) == 0L)
-                return(TRUE)
-
-            this_cfg <- do.call(pdc_configs, c(
-                this_args,
-                list(
-                    type = type,
-                    share.config = this_share
-                )))
-
-            decrease <- c(
-                partitional = 0L,
-                hierarchical = 0L,
-                fuzzy = 0L,
-                tadpole = 0L
-            )
-
-            if (is.null(this_args$none)) {
-                decrease <- decrease + 1L
-
-            } else {
-                must_decrease <- setdiff(share_base, this_share)
-                decrease[must_decrease] <- decrease[must_decrease] + 1L
-            }
-
-            must_decrease <- sapply(this_args[names(decrease)], is.null)
-
-            decrease[must_decrease] <- decrease[must_decrease] + pdc_nrows[must_decrease] - 1L
-
-            expected_nrows <- pdc_nrows - decrease
-
-            all(expected_nrows[names(this_cfg)] == sapply(this_cfg, nrow))
+        if (type == "distance") {
+            this_args$tadpole <- NULL
+            this_share <- this_share[setdiff(names(this_share), "tadpole")]
         }
-    ))
+
+        if (length(this_args) == 0L || length(this_share) == 0L)
+            return(TRUE)
+
+        this_cfg <- do.call(pdc_configs, c(
+            this_args,
+            list(
+                type = type,
+                share.config = this_share
+            )))
+
+        decrease <- c(
+            partitional = 0L,
+            hierarchical = 0L,
+            fuzzy = 0L,
+            tadpole = 0L
+        )
+
+        if (is.null(this_args$none)) {
+            decrease <- decrease + 1L
+
+        } else {
+            must_decrease <- setdiff(share_base, this_share)
+            decrease[must_decrease] <- decrease[must_decrease] + 1L
+        }
+
+        must_decrease <- sapply(this_args[names(decrease)], is.null)
+        decrease[must_decrease] <- decrease[must_decrease] + pdc_nrows[must_decrease] - 1L
+        expected_nrows <- pdc_nrows - decrease
+        all(expected_nrows[names(this_cfg)] == sapply(this_cfg, nrow))
+    }
+
+    expect_true(all(nrows_is_correct))
 })
 
 # =================================================================================================
 # compare_clusterings_configs
 # =================================================================================================
 
-test_that("Using compare_clusterings_configs works as expected.", {
+test_that("Using compare_clusterings_configs() works as expected.", {
     ## ---------------------------------------------------------- controls errors
     expect_error(compare_clusterings_configs(controls = "a"),
                  "controls.*list")
@@ -124,21 +120,22 @@ test_that("Using compare_clusterings_configs works as expected.", {
                  "centroids.*types")
 
     ## ---------------------------------------------------------- working configs
-    expect_true(all(sapply(compare_clusterings_configs(), nrow) == 1L))
-    expect_true(all(
-        1L == sapply(FUN = nrow, X = compare_clusterings_configs(
-            types = c("p", "h", "f", "t"),
-            controls = list(
-                partitional = partitional_control(),
-                hierarchical = hierarchical_control(),
-                fuzzy = fuzzy_control(),
-                tadpole = tadpole_control(dc = 1.5, window.size = 1L)
-            ),
-            preprocs = pdc_configs("p", none = list(foo = "bar")),
-            distances = pdc_configs("d", sbd = list(foo = "bar")),
-            centroids = pdc_configs("c", default = list(foo = "bar"))
-        ))
+    nrows_each_config <- sapply(compare_clusterings_configs(), nrow)
+    expect_true(all(nrows_each_config == 1L))
+
+    nrows_each_custom_config <- sapply(FUN = nrow, X = compare_clusterings_configs(
+        types = c("p", "h", "f", "t"),
+        controls = list(
+            partitional = partitional_control(),
+            hierarchical = hierarchical_control(),
+            fuzzy = fuzzy_control(),
+            tadpole = tadpole_control(dc = 1.5, window.size = 1L)
+        ),
+        preprocs = pdc_configs("p", none = list(foo = "bar")),
+        distances = pdc_configs("d", sbd = list(foo = "bar")),
+        centroids = pdc_configs("c", default = list(foo = "bar"))
     ))
+    expect_true(all(nrows_each_custom_config == 1L))
 })
 
 # =================================================================================================
