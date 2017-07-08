ff56810ada71d0bb5a55d410f7d27ac5de823a22
asardaes <alexis.sarda@gmail.com>
2017-07-08 13:57:27 +0200

    Adjust custom-dist integration tests

--- tests/testthat/integration/custom-dist.R
+++ tests/testthat/integration/custom-dist.R
@@ -12,7 +12,6 @@ ols <- ls()
 # =================================================================================================
 
 test_that("Calling dtwclust after registering a custom distance works as expected.", {
-    ## ---------------------------------------------------------- nDTW
     ndtw <- function(x, y, ...) {
         dtw::dtw(x, y, distance.only = TRUE, ...)$normalizedDistance
     }
@@ -22,53 +21,34 @@ test_that("Calling dtwclust after registering a custom distance works as expecte
                                loop = TRUE, type = "metric", distance = TRUE,
                                description = "Normalized DTW with L1 norm")
 
-    pc_ndtw <- dtwclust(data_subset, k = 4, distance = "nDTW", seed = 8319)
-    pc_ndtw2 <- tsclust(data_subset, k = 4, distance = "nDTW", seed = 8319)
-
+    ## ---------------------------------------------------------- non-symmetric
+    pc_ndtw <- tsclust(data_subset, k = 4, distance = "nDTW", seed = 8319L)
     pc_ndtw <- reset_nondeterministic(pc_ndtw)
-    pc_ndtw2 <- reset_nondeterministic(pc_ndtw2)
 
     ## ---------------------------------------------------------- symmetric
-    pc_ndtw_sym <- dtwclust(data_subset, k = 4, distance = "nDTW",
-                            seed = 8319, control = list(symmetric = TRUE))
-
-    pc_ndtw_sym2 <- tsclust(data_subset, k = 4, distance = "nDTW",
-                            seed = 8319, control = partitional_control(symmetric = TRUE))
-
+    pc_ndtw_sym <- tsclust(data_subset, k = 4, distance = "nDTW",
+                           seed = 8319, control = partitional_control(symmetric = TRUE))
     pc_ndtw_sym <- reset_nondeterministic(pc_ndtw_sym)
-    pc_ndtw_sym2 <- reset_nondeterministic(pc_ndtw_sym2)
 
     ## just for expect below
-    pc_ndtw@control@symmetric <- TRUE
-    pc_ndtw2@control$symmetric <- TRUE
-    pc_ndtw@call <- pc_ndtw_sym@call <- as.call(list("zas", a = 1))
-    pc_ndtw2@call <- pc_ndtw_sym2@call <- as.call(list("zas", a = 1))
+    pc_ndtw@control$symmetric <- TRUE
+    pc_ndtw@call <- pc_ndtw_sym@call <- as.call(list("foo", bar = 1))
     expect_identical(pc_ndtw, pc_ndtw_sym)
-    expect_identical(pc_ndtw2, pc_ndtw_sym2)
 
     ## ---------------------------------------------------------- custom params
-    pc_ndtw_par <- dtwclust(data_subset, k = 4, distance = "nDTW",
-                            seed = 8319, control = list(window.size = 18L),
-                            open.begin = TRUE, open.end = TRUE,
-                            step.pattern = asymmetric)
+    pc_ndtw_par <- tsclust(data_subset, k = 4, distance = "nDTW", seed = 8319L,
+                           args = tsclust_args(dist = list(window.size = 18L,
+                                                           open.begin = TRUE,
+                                                           open.end = TRUE,
+                                                           step.pattern = asymmetric)))
 
     pc_ndtw_par <- reset_nondeterministic(pc_ndtw_par)
 
-    expect_equal(pc_ndtw_par@distmat,
-                 proxy::dist(data_subset, data_subset, method = "nDTW",
-                             window.type = "slantedband", window.size = 18L,
-                             open.begin = TRUE, open.end = TRUE,
-                             step.pattern = asymmetric),
-                 tolerance = 0,
-                 check.attributes = FALSE)
-
-    pc_ndtw_par2 <- tsclust(data_subset, k = 4, distance = "nDTW", seed = 8319,
-                            args = tsclust_args(dist = list(window.size = 18L,
-                                                            open.begin = TRUE,
-                                                            open.end = TRUE,
-                                                            step.pattern = asymmetric)))
-
-    expect_identical(pc_ndtw_par@distmat, pc_ndtw_par2@distmat)
+    expect_equivalent(pc_ndtw_par@distmat,
+                      proxy::dist(data_subset, data_subset, method = "nDTW",
+                                  window.type = "slantedband", window.size = 18L,
+                                  open.begin = TRUE, open.end = TRUE,
+                                  step.pattern = asymmetric))
 
     assign("pc_ndtw", pc_ndtw, persistent)
     assign("pc_ndtw_sym", pc_ndtw_sym, persistent)
