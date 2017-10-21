f5b700be4181f700c60109ec2618d3124aa570ab
asardaes <alexis.sarda@gmail.com>
2017-10-21 16:45:41 +0200

    Add unit tests for NCCc()

--- tests/testthat/unit/misc.R
+++ tests/testthat/unit/misc.R
@@ -24,6 +24,34 @@ test_that("reinterpolate function works correctly for supported inputs.", {
 })
 
 # ==================================================================================================
+# NCCc
+# ==================================================================================================
+
+test_that("NCCc function works correctly for supported inputs.", {
+    x_uv <- data[[1L]]
+    x_mv <- data_multivariate[[1L]]
+    invalid_inputs <- list(
+        "NA" = NA,
+        "NULL" = NULL,
+        "char" = "1",
+        "bool" = TRUE,
+        "empty" = numeric(),
+        "miss" = c(1, NA)
+    )
+
+    expect_error(NCCc(x_uv, x_mv), regexp = "dimension")
+    expect_error(NCCc(x_mv, x_uv), regexp = "dimension")
+    expect_error(NCCc(x_mv, x_mv), regexp = "multivariate")
+    for (input in names(invalid_inputs)) {
+        expect_error(do.call("NCCc", list(x = invalid_inputs[[input]], y = x_uv)),
+                     info = paste("with", input, "input in x"))
+        expect_error(do.call("NCCc", list(x = x_uv, y = invalid_inputs[[input]])),
+                     info = paste("with", input, "input in y"))
+    }
+    expect_length(NCCc(x_uv, x_uv), 2L * length(x_uv) - 1L)
+})
+
+# ==================================================================================================
 # clean
 # ==================================================================================================
 
