0f6686cf2d5a129fa10ae2113408671caca3e5e9
asardaes <alexis.sarda@gmail.com>
2017-10-25 17:43:31 +0200

    Fix coverage script

--- codecov.R
+++ codecov.R
@@ -1,5 +1,4 @@
 library(covr)
-options(covr.exclude_pattern = TRUE, covr.exclude_start = TRUE, covr.exclude_end = TRUE)
 codecov(type = "all",
         line_exclusions = list(
             "R/pkg.R"
