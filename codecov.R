0915b99dc66017247e3c798157d348b9d5c2de73
asardaes <alexis.sarda@gmail.com>
2017-10-27 22:32:43 +0200

    Update coverage script

--- codecov.R
+++ codecov.R
@@ -1,5 +1,5 @@
-library(covr)
-codecov(type = "all",
-        line_exclusions = list(
-            "R/pkg.R"
-        ))
+library("covr")
+options(covr.exclude_pattern = rex::rex("#" %or% "//", any_spaces, "nocov"),
+        covr.exclude_start = rex::rex("#" %or% "//", any_spaces, "nocov", any_spaces, "start"),
+        covr.exclude_end = rex::rex("#" %or% "//", any_spaces, "nocov", any_spaces, "end"))
+covr::codecov(type = "tests", line_exclusions = list("R/pkg.R"))
