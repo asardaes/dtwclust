9a641135112acf1b2ae31cd03cb930724fae2070
asardaes <alexis.sarda@gmail.com>
2017-12-13 17:38:36 +0100

    Adjust system tests for partitional clustering
    Remove redundant ones and add one for fcmdd.
    Add some more info to a couple of tests.

--- tests/testthat/system/invalid-inputs.R
+++ tests/testthat/system/invalid-inputs.R
@@ -31,59 +31,6 @@ test_that("Errors in input data are detected by tsclust.", {
 })
 
 # ==================================================================================================
-# invalid distance
-# ==================================================================================================
-
-test_that("Errors in distance argument are correctly detected by tsclust.", {
-    expect_error(tsclust(data_matrix, k = 20, distance = mean), "proxy", info = "Function")
-
-    expect_error(tsclust(data_matrix, k = 20, distance = NULL), "proxy", info = "NULL")
-
-    expect_error(tsclust(data_matrix, k = 20, distance = NA), "proxy", info = "NA")
-
-    expect_error(tsclust(data_matrix, k = 20, distance = "dummy"), "proxy", info = "Unregistered")
-
-    expect_error(tsclust(data, k = 20, distance = "lbk",
-                         args = tsclust_args(dist = list(window.size = 18L))),
-                 "different length", info = "LBK")
-
-    expect_error(tsclust(data, k = 20, distance = "lbi",
-                         args = tsclust_args(dist = list(window.size = 18L))),
-                 "different length", info = "LBK")
-
-    expect_error(tsclust(data, k = 20, distance = "dtw_lb",
-                         args = tsclust_args(dist = list(window.size = 18L))),
-                 "different length", info = "DTW_LB")
-})
-
-# ==================================================================================================
-# invalid centroid
-# ==================================================================================================
-
-test_that("Errors in centroid argument are correctly detected by tsclust.", {
-    expect_error(tsclust(data, centroid = "mean"),
-                 "different length", "mean")
-
-    expect_error(tsclust(data, centroid = "mean"),
-                 "different length", "median")
-
-    expect_error(tsclust(data, centroid = "fcm"),
-                 "arg", info = "FCM is for fuzzy")
-
-    expect_error(tsclust(data, centroid = "goku"),
-                 "arg", info = "Unknown centroid")
-
-    expect_error(tsclust(data, centroid = NULL),
-                 "definition", info = "NULL centroid")
-
-    expect_error(tsclust(data, centroid = NA),
-                 "definition", info = "NA centroid")
-
-    expect_error(tsclust(data, centroid = function(x, ...) { x }),
-                 "centroid.*arguments")
-})
-
-# ==================================================================================================
 # invalid control
 # ==================================================================================================
 
@@ -164,15 +111,40 @@ test_that("Invalid combinations in hierarchical clustering are detected by tsclu
 test_that("Invalid combinations in partitional clustering are detected by tsclust.", {
     expect_error(tsclust(data, k = 101L), "more clusters")
 
-    expect_error(tsclust(data, distance = "lbk"), "different length")
-    expect_error(tsclust(data, distance = "lbi"), "different length")
-    expect_error(tsclust(data, distance = "dtw_lb"), "different length")
+    expect_error(tsclust(data_matrix, k = 20, distance = mean), "proxy", info = "Function")
+    expect_error(tsclust(data_matrix, k = 20, distance = NULL), "proxy", info = "NULL")
+    expect_error(tsclust(data_matrix, k = 20, distance = NA), "proxy", info = "NA")
+    expect_error(tsclust(data_matrix, k = 20, distance = "dummy"), "proxy", info = "Unregistered")
+    expect_error(tsclust(data, k = 20, distance = "lbk",
+                         args = tsclust_args(dist = list(window.size = 18L))),
+                 "different length", info = "LBK")
+    expect_error(tsclust(data, k = 20, distance = "lbi",
+                         args = tsclust_args(dist = list(window.size = 18L))),
+                 "different length", info = "LBK")
+    expect_error(tsclust(data, k = 20, distance = "dtw_lb",
+                         args = tsclust_args(dist = list(window.size = 18L))),
+                 "different length", info = "DTW_LB")
 
-    expect_error(tsclust(data, centroid = "mean"), "different length")
-    expect_error(tsclust(data, centroid = "median"), "different length")
-    expect_error(tsclust(data, centroid = "fcm"), "arg")
+    expect_error(tsclust(data, centroid = "mean"),
+                 "different length", info = "mean")
+    expect_error(tsclust(data, centroid = "median"),
+                 "different length", info = "median")
+    expect_error(tsclust(data, centroid = "fcm"),
+                 "arg", info = "FCM is for fuzzy")
+    expect_error(tsclust(data, centroid = "fcmdd"),
+                 "arg", info = "FCMdd is for fuzzy")
+    expect_error(tsclust(data, centroid = "goku"),
+                 "arg", info = "Unknown centroid")
+    expect_error(tsclust(data, centroid = NULL),
+                 "definition", info = "NULL centroid")
+    expect_error(tsclust(data, centroid = NA),
+                 "definition", info = "NA centroid")
+    expect_error(tsclust(data, centroid = function(x, ...) { x }),
+                 "centroid.*arguments",
+                 info = "custom centroid function requires specific formal parameters")
 
-    expect_error(tsclust(data, preproc = "zscore"), "preprocessing")
+    expect_error(tsclust(data, preproc = "zscore"), "preprocessing",
+                 info = "got char instead of function")
 })
 
 # ==================================================================================================
