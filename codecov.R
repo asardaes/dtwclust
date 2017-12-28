06a39191d3d0eacf812f7488d242cfb44a44e0e5
asardaes <alexis.sarda@gmail.com>
2017-12-28 02:20:39 +0100

    Ignore coverage of src headers

--- codecov.R
+++ codecov.R
@@ -2,4 +2,8 @@ library("covr")
 options(covr.exclude_pattern = rex::rex("#" %or% "//", any_spaces, "nocov"),
         covr.exclude_start = rex::rex("#" %or% "//", any_spaces, "nocov", any_spaces, "start"),
         covr.exclude_end = rex::rex("#" %or% "//", any_spaces, "nocov", any_spaces, "end"))
-covr::codecov(type = "tests", line_exclusions = list("R/pkg.R"))
+covr::codecov(type = "tests", line_exclusions = list(
+    "R/pkg.R",
+    "src/dtwclust.h",
+    "src/dtwclust++.h"
+))
