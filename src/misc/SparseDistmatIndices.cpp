e5b9fa3c4e614d05fd1a119c9384e88749aa10d0
asardaes <alexis.sarda@gmail.com>
2018-01-06 19:15:11 +0100

    Adjust names of private members

--- src/misc/SparseDistmatIndices.cpp
+++ src/misc/SparseDistmatIndices.cpp
@@ -6,16 +6,19 @@
 
 namespace dtwclust {
 
-class SparseDistmatIndices {
+class SparseDistmatIndices
+{
 public:
-    SparseDistmatIndices(int num_rows) : _num_rows(num_rows) {}
+    SparseDistmatIndices(int num_rows)
+        : num_rows_(num_rows)
+    { }
 
-    Rcpp::IntegerMatrix getNewIndices(const Rcpp::IntegerVector& i,
-                                      const Rcpp::IntegerVector& j,
-                                      const bool symmetric)
+    Rcpp::IntegerMatrix getNewIndices(
+            const Rcpp::IntegerVector& i,
+            const Rcpp::IntegerVector& j,
+            const bool symmetric)
     {
         std::vector<int> new_i, new_j;
-
         for (int ii = 0; ii < i.length(); ii++) {
             for (int jj = 0; jj < j.length(); jj++) {
                 int this_i = i[ii];
@@ -25,43 +28,41 @@ public:
                     this_i = this_j;
                     this_j = swap;
                 }
-                int ij = this_i + (this_j - 1) * _num_rows;
-                bool new_index = existing_indices.find(ij) == existing_indices.end();
+                int ij = this_i + (this_j - 1) * num_rows_;
+                bool new_index = existing_indices_.find(ij) == existing_indices_.end();
                 if (new_index) {
-                    existing_indices.insert(ij);
+                    existing_indices_.insert(ij);
                     new_i.push_back(this_i);
                     new_j.push_back(this_j);
                 }
             }
         }
-
         Rcpp::IntegerMatrix new_ids(new_i.size(), 2);
         for (int k = 0; k < new_ids.nrow(); k++) {
             new_ids(k, 0) = new_i[k];
             new_ids(k, 1) = new_j[k];
         }
-
         return new_ids;
     }
 
 private:
-    int _num_rows;
-    std::unordered_set<int> existing_indices;
+    int num_rows_;
+    std::unordered_set<int> existing_indices_;
 };
 
-RcppExport SEXP SparseDistmatIndices__new(SEXP num_rows) {
+RcppExport SEXP SparseDistmatIndices__new(SEXP num_rows)
+{
     BEGIN_RCPP
     Rcpp::XPtr<SparseDistmatIndices> ptr(new SparseDistmatIndices(Rcpp::as<int>(num_rows)), true);
     return ptr;
     END_RCPP
 }
 
-RcppExport SEXP SparseDistmatIndices__getNewIndices(SEXP xptr, SEXP i, SEXP j, SEXP symmetric) {
+RcppExport SEXP SparseDistmatIndices__getNewIndices(SEXP xptr, SEXP i, SEXP j, SEXP symmetric)
+{
     BEGIN_RCPP
     Rcpp::XPtr<SparseDistmatIndices> ptr(xptr);
-    return ptr->getNewIndices(Rcpp::IntegerVector(i),
-                              Rcpp::IntegerVector(j),
-                              Rcpp::as<bool>(symmetric));
+    return ptr->getNewIndices(i, j, Rcpp::as<bool>(symmetric));
     END_RCPP
 }
 
