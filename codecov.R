4e7134b26c9d9cd3db6fc061eade70b05a246dd0
Alexis Sardá <asardaes@users.noreply.github.com>
2018-01-18 09:12:21 +0100

    Use verbose coverage in TravisCI

--- codecov.R
+++ codecov.R
@@ -2,6 +2,6 @@ library("covr")
 options(covr.exclude_pattern = rex::rex("#" %or% "//", any_spaces, "nocov"),
         covr.exclude_start = rex::rex("#" %or% "//", any_spaces, "nocov", any_spaces, "start"),
         covr.exclude_end = rex::rex("#" %or% "//", any_spaces, "nocov", any_spaces, "end"))
-covr::codecov(type = "tests", line_exclusions = list(
+covr::codecov(type = "tests", quiet = FALSE, clean = FALSE, line_exclusions = list(
     "R/pkg.R"
 ))
