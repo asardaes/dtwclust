63986d909b1a32767bd9fb1e7a85e2e10c5bfaa7
asardaes <alexis.sarda@gmail.com>
2018-01-21 21:46:01 +0100

    Update centroid's timing experiments

--- timing-experiments/30-prototyping-experiments.R
+++ timing-experiments/30-prototyping-experiments.R
@@ -40,12 +40,16 @@ series_mv_normalized <- lapply(series_mv, zscore)
 # Number of series to consider during prototyping
 num_series <- seq(from = 10L, to = 100L, by = 10L)
 # Window sizes for the experiments (where applicable)
-window_sizes <- seq(from = 10L, to = 100L, by = 10L)
+window_sizes <- seq(from = 30L, to = 90L, by = 30L)
 # Number of times each experiment will be repeated (by microbenchmark)
-times <- if (short_experiments) 10L else 50L
+times <- 30L
+# Number of threads to test for those that support multi-threading
+num_threads <- c(1L, 2L, 4L)
 
 if (short_experiments) {
+    times <- 10L
     window_sizes <- num_series <- c(10L, 20L)
+    num_threads <- c(2L, 4L)
 }
 
 #' NOTE: all experiments are pretty much equivalent. They are run within a new environment so that
@@ -130,34 +134,43 @@ cat("\tRunning dba experiments for univariate series\n")
 cent_dba_univariate <- with(
     new.env(),
     {
-        # Loop along extracted subsets
-        benchmarks <- lapply(series, function(this_series) {
-            # Build expressions to evaluate, substituting number of series and window size
-            expressions <- lapply(num_series, function(ns) {
-                lapply(window_sizes, function(ws) {
-                    bquote(
-                        DBA(this_series[1L:.(ns)], ref, window.size = .(ws), error.check = FALSE)
-                    )
+        # Loop across number of threads to test
+        dplyr::bind_rows(lapply(num_threads, function(num_threads) {
+            cat("\t\t")
+            RcppParallel::setThreadOptions(num_threads)
+
+            # Loop along extracted subsets
+            benchmarks <- lapply(series, function(this_series) {
+                # Build expressions to evaluate, substituting number of series and window size
+                expressions <- lapply(num_series, function(ns) {
+                    lapply(window_sizes, function(ws) {
+                        bquote(
+                            DBA(this_series[1L:.(ns)], ref, window.size = .(ws), error.check = FALSE)
+                        )
+                    })
                 })
+                expressions <- unlist(expressions, recursive = FALSE)
+
+                # Extract reference series
+                ref <- this_series[[length(this_series)]]
+
+                # Evaluate expressions
+                benchmark <- summary(microbenchmark(list = expressions, times = times, unit = "ms"))
+
+                # Return data frame with results
+                cat(".")
+                data.frame(cent = "dba_univariate",
+                           num_threads = num_threads,
+                           series_length = NROW(ref),
+                           window_size = window_sizes,
+                           num_series = rep(num_series, each = length(window_sizes)),
+                           median_time_ms = benchmark$median)
             })
-            expressions <- unlist(expressions, recursive = FALSE)
 
-            # Extract reference series
-            ref <- this_series[[length(this_series)]]
-
-            # Evaluate expressions
-            benchmark <- summary(microbenchmark(list = expressions, times = times, unit = "ms"))
-
-            # Return data frame with results
-            data.frame(cent = "dba_univariate",
-                       series_length = NROW(ref),
-                       window_size = window_sizes,
-                       num_series = rep(num_series, each = length(window_sizes)),
-                       median_time_ms = benchmark$median)
-        })
-
-        # Bind results for all series and return to global environment
-        dplyr::bind_rows(benchmarks)
+            cat("\n")
+            # Bind results for all series
+            dplyr::bind_rows(benchmarks)
+        }))
     })
 
 # --------------------------------------------------------------------------------------------------
@@ -168,35 +181,44 @@ cat("\tRunning dba experiments for multivariate series (by-series)\n")
 cent_dba_multivariate_byS <- with(
     new.env(),
     {
-        # Loop along extracted subsets
-        benchmarks <- lapply(series_mv, function(this_series) {
-            # Build expressions to evaluate, substituting number of series and window size
-            expressions <- lapply(num_series, function(ns) {
-                lapply(window_sizes, function(ws) {
-                    bquote(
-                        DBA(this_series[1L:.(ns)], ref, window.size = .(ws),
-                            mv.ver = "by-s", error.check = FALSE)
-                    )
+        # Loop across number of threads to test
+        dplyr::bind_rows(lapply(num_threads, function(num_threads) {
+            cat("\t\t")
+            RcppParallel::setThreadOptions(num_threads)
+
+            # Loop along extracted subsets
+            benchmarks <- lapply(series_mv, function(this_series) {
+                # Build expressions to evaluate, substituting number of series and window size
+                expressions <- lapply(num_series, function(ns) {
+                    lapply(window_sizes, function(ws) {
+                        bquote(
+                            DBA(this_series[1L:.(ns)], ref, window.size = .(ws),
+                                mv.ver = "by-s", error.check = FALSE)
+                        )
+                    })
                 })
+                expressions <- unlist(expressions, recursive = FALSE)
+
+                # Extract reference series
+                ref <- this_series[[length(this_series)]]
+
+                # Evaluate expressions
+                benchmark <- summary(microbenchmark(list = expressions, times = times, unit = "ms"))
+
+                # Return data frame with results
+                cat(".")
+                data.frame(cent = "dba_multivariate_byS",
+                           num_threads = num_threads,
+                           series_length = NROW(ref),
+                           window_size = window_sizes,
+                           num_series = rep(num_series, each = length(window_sizes)),
+                           median_time_ms = benchmark$median)
             })
-            expressions <- unlist(expressions, recursive = FALSE)
 
-            # Extract reference series
-            ref <- this_series[[length(this_series)]]
-
-            # Evaluate expressions
-            benchmark <- summary(microbenchmark(list = expressions, times = times, unit = "ms"))
-
-            # Return data frame with results
-            data.frame(cent = "dba_multivariate_byS",
-                       series_length = NROW(ref),
-                       window_size = window_sizes,
-                       num_series = rep(num_series, each = length(window_sizes)),
-                       median_time_ms = benchmark$median)
-        })
-
-        # Bind results for all series and return to global environment
-        dplyr::bind_rows(benchmarks)
+            cat("\n")
+            # Bind results for all series and return to global environment
+            dplyr::bind_rows(benchmarks)
+        }))
     })
 
 # --------------------------------------------------------------------------------------------------
@@ -207,35 +229,44 @@ cat("\tRunning dba experiments for multivariate series (by-variable)\n")
 cent_dba_multivariate_byV <- with(
     new.env(),
     {
-        # Loop along extracted subsets
-        benchmarks <- lapply(series_mv, function(this_series) {
-            # Build expressions to evaluate, substituting number of series and window size
-            expressions <- lapply(num_series, function(ns) {
-                lapply(window_sizes, function(ws) {
-                    bquote(
-                        DBA(this_series[1L:.(ns)], ref, window.size = .(ws),
-                            mv.ver = "by-v", error.check = FALSE)
-                    )
+        # Loop across number of threads to test
+        dplyr::bind_rows(lapply(num_threads, function(num_threads) {
+            cat("\t\t")
+            RcppParallel::setThreadOptions(num_threads)
+
+            # Loop along extracted subsets
+            benchmarks <- lapply(series_mv, function(this_series) {
+                # Build expressions to evaluate, substituting number of series and window size
+                expressions <- lapply(num_series, function(ns) {
+                    lapply(window_sizes, function(ws) {
+                        bquote(
+                            DBA(this_series[1L:.(ns)], ref, window.size = .(ws),
+                                mv.ver = "by-v", error.check = FALSE)
+                        )
+                    })
                 })
+                expressions <- unlist(expressions, recursive = FALSE)
+
+                # Extract reference series
+                ref <- this_series[[length(this_series)]]
+
+                # Evaluate expressions
+                benchmark <- summary(microbenchmark(list = expressions, times = times, unit = "ms"))
+
+                # Return data frame with results
+                cat(".")
+                data.frame(cent = "dba_multivariate_byV",
+                           num_threads = num_threads,
+                           series_length = NROW(ref),
+                           window_size = window_sizes,
+                           num_series = rep(num_series, each = length(window_sizes)),
+                           median_time_ms = benchmark$median)
             })
-            expressions <- unlist(expressions, recursive = FALSE)
 
-            # Extract reference series
-            ref <- this_series[[length(this_series)]]
-
-            # Evaluate expressions
-            benchmark <- summary(microbenchmark(list = expressions, times = times, unit = "ms"))
-
-            # Return data frame with results
-            data.frame(cent = "dba_multivariate_byV",
-                       series_length = NROW(ref),
-                       window_size = window_sizes,
-                       num_series = rep(num_series, each = length(window_sizes)),
-                       median_time_ms = benchmark$median)
-        })
-
-        # Bind results for all series and return to global environment
-        dplyr::bind_rows(benchmarks)
+            cat("\n")
+            # Bind results for all series and return to global environment
+            dplyr::bind_rows(benchmarks)
+        }))
     })
 
 # --------------------------------------------------------------------------------------------------
@@ -246,30 +277,40 @@ cat("\tRunning sdtw_cent experiments for univariate series\n")
 cent_sdtw_univariate <- with(
     new.env(),
     {
-        # Loop along extracted subsets
-        benchmarks <- lapply(series, function(this_series) {
-            # Build expressions to evaluate, substituting number of series
-            expressions <- lapply(num_series, function(ns) {
-                bquote(
-                    sdtw_cent(this_series[1L:.(ns)], ref, error.check = FALSE)
-                )
+        # Loop across number of threads to test
+        dplyr::bind_rows(lapply(num_threads, function(num_threads) {
+            cat("\t\t")
+            RcppParallel::setThreadOptions(num_threads)
+
+            # Loop along subset of extracted subsets
+            series <- series[seq(from = 1L, to = length(series), by = 2L)]
+            benchmarks <- lapply(series, function(this_series) {
+                # Build expressions to evaluate, substituting number of series
+                expressions <- lapply(num_series, function(ns) {
+                    bquote(
+                        sdtw_cent(this_series[1L:.(ns)], ref, error.check = FALSE, num_threads = num_threads)
+                    )
+                })
+
+                # Extract reference series
+                ref <- this_series[[length(this_series)]]
+
+                # Evaluate expressions
+                benchmark <- summary(microbenchmark(list = expressions, times = times, unit = "ms"))
+
+                # Return data frame with results
+                cat(".")
+                data.frame(cent = "sdtw_cent_univariate",
+                           num_threads = num_threads,
+                           series_length = NROW(ref),
+                           num_series = num_series,
+                           median_time_ms = benchmark$median)
             })
 
-            # Extract reference series
-            ref <- this_series[[length(this_series)]]
-
-            # Evaluate expressions
-            benchmark <- summary(microbenchmark(list = expressions, times = times, unit = "ms"))
-
-            # Return data frame with results
-            data.frame(cent = "sdtw_cent_univariate",
-                       series_length = NROW(ref),
-                       num_series = num_series,
-                       median_time_ms = benchmark$median)
-        })
-
-        # Bind results for all series and return to global environment
-        dplyr::bind_rows(benchmarks)
+            cat("\n")
+            # Bind results for all series and return to global environment
+            dplyr::bind_rows(benchmarks)
+        }))
     })
 
 # --------------------------------------------------------------------------------------------------
@@ -280,30 +321,40 @@ cat("\tRunning sdtw_cent experiments for multivariate series\n")
 cent_sdtw_multivariate <- with(
     new.env(),
     {
-        # Loop along extracted subsets
-        benchmarks <- lapply(series_mv, function(this_series) {
-            # Build expressions to evaluate, substituting number of series
-            expressions <- lapply(num_series, function(ns) {
-                bquote(
-                    sdtw_cent(this_series[1L:.(ns)], ref, error.check = FALSE)
-                )
+        # Loop across number of threads to test
+        dplyr::bind_rows(lapply(num_threads, function(num_threads) {
+            cat("\t\t")
+            RcppParallel::setThreadOptions(num_threads)
+
+            # Loop along subset of extracted subsets
+            series_mv <- series_mv[seq(from = 1L, to = length(series_mv), by = 2L)]
+            benchmarks <- lapply(series_mv, function(this_series) {
+                # Build expressions to evaluate, substituting number of series
+                expressions <- lapply(num_series, function(ns) {
+                    bquote(
+                        sdtw_cent(this_series[1L:.(ns)], ref, error.check = FALSE, num_threads = num_threads)
+                    )
+                })
+
+                # Extract reference series
+                ref <- this_series[[length(this_series)]]
+
+                # Evaluate expressions
+                benchmark <- summary(microbenchmark(list = expressions, times = times, unit = "ms"))
+
+                # Return data frame with results
+                cat(".")
+                data.frame(cent = "sdtw_cent_multivariate",
+                           num_threads = num_threads,
+                           series_length = NROW(ref),
+                           num_series = num_series,
+                           median_time_ms = benchmark$median)
             })
 
-            # Extract reference series
-            ref <- this_series[[length(this_series)]]
-
-            # Evaluate expressions
-            benchmark <- summary(microbenchmark(list = expressions, times = times, unit = "ms"))
-
-            # Return data frame with results
-            data.frame(cent = "sdtw_cent_multivariate",
-                       series_length = NROW(ref),
-                       num_series = num_series,
-                       median_time_ms = benchmark$median)
-        })
-
-        # Bind results for all series and return to global environment
-        dplyr::bind_rows(benchmarks)
+            cat("\n")
+            # Bind results for all series and return to global environment
+            dplyr::bind_rows(benchmarks)
+        }))
     })
 
 # --------------------------------------------------------------------------------------------------
