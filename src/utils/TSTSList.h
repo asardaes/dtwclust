a9dcf771e245e9000af5a5b09e068f9f25d61778
asardaes <alexis.sarda@gmail.com>
2018-02-02 18:57:54 +0100

    Use private shared_ptr to vector
    Even less copies in clones of the distance calculators.

--- src/utils/TSTSList.h
+++ src/utils/TSTSList.h
@@ -2,6 +2,7 @@
 #define DTWCLUST_TSTSLIST_HPP_
 
 #include <complex>
+#include <memory> // make_shared, shared_ptr
 #include <type_traits> // conditional, is_same
 #include <vector>
 
@@ -29,20 +30,21 @@ public:
     // constructors
     TSTSList() {} // dummy
     TSTSList(const Rcpp::List& series)
+        : series_(std::make_shared<std::vector<TSTSType>>())
     {
         for (const SEXP& x : series) {
             SeriesType x_rcpp(x);
-            series_.push_back(TSTSType(x_rcpp));
+            series_->push_back(TSTSType(x_rcpp));
         }
     }
     // operator[]
-    TSTSType& operator[](const int i) { return series_[i]; }
-    const TSTSType& operator[](const int i) const { return series_[i]; }
+    TSTSType& operator[](const int i) { return (*series_)[i]; }
+    const TSTSType& operator[](const int i) const { return (*series_)[i]; }
     // length
-    int length() const { return series_.size(); }
+    int length() const { return series_->size(); }
 
 private:
-    std::vector<TSTSType> series_;
+    std::shared_ptr<std::vector<TSTSType>> series_;
 };
 
 // specialization for complex vectors
@@ -54,23 +56,24 @@ public:
     // constructors
     TSTSList() {} // dummy
     TSTSList(const Rcpp::List& series)
+        : series_(std::make_shared<std::vector<TSTSType>>())
     {
         for (const SEXP& x : series) {
             Rcpp::ComplexVector x_rcpp(x);
             // see http://rcpp-devel.r-forge.r-project.narkive.com/o5ubHVos/multiplication-of-complexvector
             arma::cx_vec x_arma(reinterpret_cast<std::complex<double>*>(x_rcpp.begin()),
                                 x_rcpp.length(), false, true);
-            series_.push_back(x_arma);
+            series_->push_back(x_arma);
         }
     }
     // operator[]
-    TSTSType& operator[](const int i) { return series_[i]; }
-    const TSTSType& operator[](const int i) const { return series_[i]; }
+    TSTSType& operator[](const int i) { return (*series_)[i]; }
+    const TSTSType& operator[](const int i) const { return (*series_)[i]; }
     // length
-    int length() const { return series_.size(); }
+    int length() const { return series_->size(); }
 
 private:
-    std::vector<TSTSType> series_;
+    std::shared_ptr<std::vector<TSTSType>> series_;
 };
 
 } // namespace dtwclust
