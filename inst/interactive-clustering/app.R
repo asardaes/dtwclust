5b4559b62e40dc34b1cdb2c32d2d4de66ae46ef9
asardaes <alexis.sarda@gmail.com>
2018-02-17 18:32:50 +0100

    Check for centered and scaled

--- inst/interactive-clustering/app.R
+++ inst/interactive-clustering/app.R
@@ -354,6 +354,20 @@ server <- function(input, output, session) {
             "Amount of series" = length(.series_),
             "Multivariate" = is_multivariate(.series_),
             "Same lengths" = all(diff(lengths(.series_)) == 0L),
+            "Centered" = all(sapply(.series_, function(series) {
+                if (is.null(dim(series)))
+                    mu <- mean(series)
+                else
+                    mu <- colMeans(series)
+                all(sapply(mu, function(m) { isTRUE(all.equal(0, m)) }))
+            })),
+            "Scaled" = all(sapply(.series_, function(series) {
+                if (is.null(dim(series)))
+                    sigma <- sd(series)
+                else
+                    sigma <- apply(series, 2L, sd)
+                all(sapply(sigma, function(s) { isTRUE(all.equal(1, s)) }))
+            })),
             check.names = FALSE
         )
     })
