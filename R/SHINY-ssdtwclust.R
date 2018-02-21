de52a8dd454f031145d284d425f57a72bfad2e52
asardaes <alexis.sarda@gmail.com>
2018-02-21 23:28:56 +0100

    Update documentation

--- R/SHINY-ssdtwclust.R
+++ R/SHINY-ssdtwclust.R
@@ -1,6 +1,6 @@
 #' A shiny app for semi-supervised DTW-based clustering
 #'
-#' Display a shiny user interface that implements the approach in .
+#' Display a shiny user interface that implements the approach in Dau et al. (2016).
 #'
 #' @export
 #' @importFrom shiny runApp
@@ -9,6 +9,21 @@
 #'
 #' @param series Time series in the formats accepted by [compare_clusterings()].
 #' @param ... More arguments for [shiny::runApp()].
+#'
+#' @details
+#'
+#' The approach suggested in Dau et al. (2016) argues that finding a good value of `window.size` for
+#' the DTW distance is very important, and presents how to find one by using user-provided feedback.
+#' After clustering is done, a pair of series is presented at a time, and the user must annotate the
+#' pair as:
+#'
+#' - Must link: the series should be in the same cluster.
+#' - Cannot link: the series should *not* be in the same cluster.
+#' - Skip: the choice is unclear.
+#'
+#' After each step, a good value of the window size is suggested by evaluating which clusterings
+#' fulfill the constraint(s) so far, and how (see Dau et al. (2016) for more information). The
+#' (main) procedure is thus interactive and can be abandoned at any point.
 #'
 #' @section Explore:
 #'
@@ -19,18 +34,39 @@
 #'
 #' @section Cluster:
 #'
-#'   The `window.size` parameter should *not* be specified in the extra parameters, it will be
-#'   replaced with the computed values based on the slider. Using [dba()] centroid is detected, and
-#'   will use the same window sizes.
-#'
-#'   Since [compare_clusterings()] supports parallelization with [foreach::foreach()], you can
+#'   This part of the app implements the main procedure by leveraging [compare_clusterings()]. The
+#'   interface is similar to [interactive_clustering()], so it is worth checking its documentation
+#'   too. Since [compare_clusterings()] supports parallelization with [foreach::foreach()], you can
 #'   register a parallel backend before opening the shiny app, but you should pre-load the workers
 #'   with the necessary packages and/or functions. See [parallel::clusterEvalQ()] and
 #'   [parallel::clusterExport()], as well as the examples below.
 #'
+#'   The range of window sizes is specified with the slider, and represents the size as a percentage
+#'   of the shortest series' length. The `step` parameter indicates how spaced should the sizes be
+#'   (parameter `'by'` in [base::seq()]). The `window.size` should *not* be specified in the extra
+#'   parameters, it will be replaced with the computed values based on the slider. Using [dba()]
+#'   centroid is detected, and will use the same window sizes.
+#'
+#'   For partitional clusterings with many repetitions and hierarchical clusterings with many
+#'   linkage methods, the resulting partitions can be aggregated by calling [clue::cl_medoid()] with
+#'   the specified `method`.
+#'
 #' @section Evaluate:
 #'
+#'   This section provides numerical results for reference.
+#'
+#' @note
+#'
+#' The optimization mentioned in section 3.4 of Dau et al. (2016) is also implemented here.
+#'
 #' @author Alexis Sarda-Espinosa
+#'
+#' @references
+#'
+#' Dau, H. A., Begum, N., & Keogh, E. (2016). Semi-supervision dramatically improves time series
+#' clustering under dynamic time warping. In Proceedings of the 25th ACM International on Conference
+#' on Information and Knowledge Management (pp. 999-1008). ACM.
+#' \url{https://sites.google.com/site/dtwclustering/}
 #'
 #' @examples
 #'
