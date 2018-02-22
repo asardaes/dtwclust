e0d74034109c5f3bac3db2498b3f2778963d146f
asardaes <alexis.sarda@gmail.com>
2018-02-22 22:23:33 +0100

    Adjust coverage scripts

--- codecov.R
+++ codecov.R
@@ -5,14 +5,8 @@ options(covr.exclude_pattern = rex::rex("#" %or% "//", any_spaces, "nocov"),
         covr.exclude_end = rex::rex("#" %or% "//", any_spaces, "nocov", any_spaces, "end"),
         covr.gcov = "gcov-5", # see https://github.com/r-lib/covr/issues/176
         covr.flags = sapply(covr_flags, function(dummy) { "--coverage" }))
-covr::codecov(type = "tests", quiet = FALSE, clean = FALSE, line_exclusions = list(
-    "R/pkg.R",
-    "R/SHINY-interactive-clustering.R"
-))
+covr::codecov(type = "tests", quiet = FALSE)
 
 #' to run locally, comment the line with covr.gcov above, run all code above until codecov(), and:
 #'
-#' Sys.setenv(NOT_CRAN = "true"); covr::report(package_coverage(type = "tests", quiet = FALSE, line_exclusions = list(
-#'     "R/pkg.R",
-#'     "R/SHINY-interactive-clustering.R"
-#' )))
+#' Sys.setenv(NOT_CRAN = "true"); covr::report(package_coverage(type = "tests", quiet = FALSE))
