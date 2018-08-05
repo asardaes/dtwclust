63052b3d252f64bc79566db7b401be6a819d7f32
asardaes <alexis.sarda@gmail.com>
2018-08-05 19:02:16 +0200

    Update coverage script

--- codecov.R
+++ codecov.R
@@ -3,7 +3,7 @@ covr_flags <- getOption("covr.flags")
 options(covr.exclude_pattern = rex::rex("#" %or% "//", any_spaces, "nocov"),
         covr.exclude_start = rex::rex("#" %or% "//", any_spaces, "nocov", any_spaces, "start"),
         covr.exclude_end = rex::rex("#" %or% "//", any_spaces, "nocov", any_spaces, "end"),
-        covr.gcov = "gcov-5", # see https://github.com/r-lib/covr/issues/176
+        covr.gcov = "gcov-7", # see https://github.com/r-lib/covr/issues/176
         covr.flags = sapply(covr_flags, function(dummy) { "--coverage" }))
 covr::codecov(type = "tests", quiet = FALSE)
 
